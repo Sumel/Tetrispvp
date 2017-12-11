b5f99f0ef85c1d3dc54a0140dffb2873e4f3ec6f
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-12-11 21:01:44 +0100

    Added clearLine method to MutableBoard interface.

--- src/main/java/tetrispvp/board/MutableBoard.java
+++ src/main/java/tetrispvp/board/MutableBoard.java
@@ -3,5 +3,6 @@ package tetrispvp.board;
 public interface MutableBoard extends Board {
     void flipBoard();
     void addLine(int lineNumber, BoardField field);
+    void clearLine(int lineNumber);
     void setFieldAtPosition(BoardField field, int x, int y);
 }
