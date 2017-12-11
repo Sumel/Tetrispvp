9c6fc7e8a5b4ddb2a56d8273139d8a50678f2b96
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-12-11 20:45:46 +0100

    Renamed addStateChangedListener to addBoardStateChangedListener in Board interface.

--- src/main/java/tetrispvp/board/Board.java
+++ src/main/java/tetrispvp/board/Board.java
@@ -5,6 +5,6 @@ import java.util.List;
 public interface Board {
     BoardField getFieldAtPosition(int x, int y);
     List<List<BoardField>> getBoardState();
-    void addStateChangedListener(BoardStateChangedListener newListener);
+    void addBoardStateChangedListener(BoardStateChangedListener newListener);
     void addLineClearedListener(LineClearedListener newListener);
 }
