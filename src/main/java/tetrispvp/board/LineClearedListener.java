bd9d28f0191fe686f4e87d10ba5968bb7e92bd91
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2017-12-19 20:16:51 +0100

    Changed LineClearedListener to return a list of cleared lines.

--- src/main/java/tetrispvp/board/LineClearedListener.java
+++ src/main/java/tetrispvp/board/LineClearedListener.java
@@ -1,5 +1,14 @@
 package tetrispvp.board;
 
+import java.util.List;
+
+
 public interface LineClearedListener {
-    void lineCleared(int linesNumber, int topPosition);
+
+    interface Line {
+        int getLineNumber();
+        List<GridField> getFieldsInLine();
+    }
+
+    void lineCleared(List<Line> clearedLines);
 }
