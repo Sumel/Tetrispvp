35e64866b61dc7375f9472dc867ad11fc5e832dd
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2018-01-02 19:54:37 +0100

    Changed CollisionChecker to accept list of colliding points as an argument, in order to fill it with data.

--- src/main/java/tetrispvp/board/CollisionChecker.java
+++ src/main/java/tetrispvp/board/CollisionChecker.java
@@ -4,8 +4,10 @@ package tetrispvp.board;
 import tetrispvp.board.Mocks.Block;
 
 import java.awt.*;
+import java.util.*;
 
 public interface CollisionChecker {
     boolean positionWithinBounds(int column, int row);
+    boolean collides(Point position, Block block, java.util.List<Point> collisionPoints);
     boolean collides(Point position, Block block);
 }
--- src/main/java/tetrispvp/board/NormalCollisionChecker.java
+++ src/main/java/tetrispvp/board/NormalCollisionChecker.java
@@ -4,6 +4,7 @@ package tetrispvp.board;
 import tetrispvp.board.Mocks.Block;
 
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NormalCollisionChecker implements CollisionChecker {
@@ -34,29 +35,38 @@ public class NormalCollisionChecker implements CollisionChecker {
     }
 
     @Override
-    public boolean collides(Point position, Block block) {
+    public boolean collides(Point position, Block block, List<Point> collisionPoints) {
         List<List<GridField>> blockFields = block.getBoardFields();
         List<List<GridField>> boardFields = board.getBoardState();
+        if (collisionPoints == null) {
+            collisionPoints = new ArrayList<Point>();
+        }
         for (int i = 0; i < blockFields.size(); ++i) {
             for (int j = 0; j < blockFields.get(i).size(); j++) {
                 GridField currentBlockField = blockFields.get(i).get(j);
                 if (currentBlockField.isOccupied()) {
                     int row = i + position.y;
                     int column = j + position.x;
-                    if (!positionWithinBounds(column, row)){
-                        return true;
-                    }
-                    if(row >= 0){
+                    Point currentPoint = new Point(column, row);
+                    if (!positionWithinBounds(column, row)) {
+                        //collision with bounds
+                        collisionPoints.add(currentPoint);
+                    } else if (row >= 0) {
                         GridField boardField = boardFields.get(row).get(column);
                         if (boardField.isOccupied() && boardField.isLocked()) {
-                            //collision
-                            return true;
+                            //collision with another field
+                            collisionPoints.add(currentPoint);
                         }
                     }
 
                 }
             }
         }
-        return false;
+        return collisionPoints.size() > 0;
+    }
+
+    @Override
+    public boolean collides(Point position, Block block) {
+        return collides(position, block, null);
     }
 }
