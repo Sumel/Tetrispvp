175db213b24ac6e6fb5124ab7ee22764b43f7225
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2018-01-07 17:34:25 +0100

    Implemented Line interface (BoardLine)

---
+++ src/main/java/tetrispvp/board/BoardLine.java
@@ -0,0 +1,24 @@
+package tetrispvp.board;
+
+
+import java.util.List;
+
+public class BoardLine implements LinesClearedListener.Line {
+    private final int lineNumber;
+    private final List<GridField> fields;
+
+    public BoardLine(int lineNumber, List<GridField> fields){
+        this.lineNumber = lineNumber;
+        this.fields = fields;
+    }
+
+    @Override
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    @Override
+    public List<GridField> getFieldsInLine() {
+        return fields;
+    }
+}
