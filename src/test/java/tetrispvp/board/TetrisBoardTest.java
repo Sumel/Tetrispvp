c276c005aecba118337a48332643ce26fc129fbb
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2018-01-08 21:46:05 +0100

    added flip test to tetris board test

--- src/test/java/tetrispvp/board/TetrisBoardTest.java
+++ src/test/java/tetrispvp/board/TetrisBoardTest.java
@@ -1,7 +1,11 @@
 package tetrispvp.board;
 
 import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
 import tetrispvp.board.Mocks.Block;
+
+import java.awt.*;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -90,6 +94,37 @@ public class TetrisBoardTest {
         assertTrue(board.isBlockCollidingBelow());
 
         verify(mover, times(1)).isBlockCollidingBelow();
+    }
+
+    private List<List<GridField>> setUpBoardForFlip() {
+        List<List<GridField>> ret = TestingUtils.getEmptyBoard();
+        BoardField field = new BoardField(true, true, Color.black, -1);
+        ret.get(19).set(4, field);
+        ret.get(18).set(4, field);
+        ret.get(17).set(4, field);
+        ret.get(17).set(5, field);
+        return ret;
+    }
+
+    @Test
+    public void flipTest() throws Exception {
+        BlockMover mover = mock(BlockMover.class);
+        TetrisBoard board = new TetrisBoard(mover);
+        Whitebox.setInternalState(board, "boardFields", setUpBoardForFlip());
+        board.flipBoard();
+        for (int row = 0; row < board.getHeight(); ++row) {
+            for (int col = 0; col < board.getWidth(); ++col) {
+                if (    row == 19 && col == 5 ||
+                        row == 19 && col == 4 ||
+                        row == 18 && col == 4 ||
+                        row == 17 && col == 4) {
+                    assertEquals(board.getFieldAtPosition(col, row), new BoardField(true, true, Color.black, -1));
+                }
+                else {
+                    assertEquals(board.getFieldAtPosition(col, row), BoardField.GetEmptyBoardField());
+                }
+            }
+        }
     }
 
 }
