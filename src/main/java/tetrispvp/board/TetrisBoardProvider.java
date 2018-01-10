26635d4b59becac9a43631018a182f1faa78bfc7
Samuel Tomczyk <samuel.tomczyk007@gmail.com>
2018-01-10 22:25:10 +0100

    Created TetrisBoardProvider with static method to construct the board.

---
+++ src/main/java/tetrispvp/board/TetrisBoardProvider.java
@@ -0,0 +1,18 @@
+package tetrispvp.board;
+
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class TetrisBoardProvider {
+    private static TetrisBoard board = null;
+    public static TetrisBoard getTetrisBoard(){
+        if(board != null){
+            return board;
+        }
+        Injector injector = Guice.createInjector(new TetrisBoardModule());
+        board = injector.getInstance(TetrisBoard.class);
+        injector.getInstance(GreyLinesManager.class);
+        return board;
+    }
+}
