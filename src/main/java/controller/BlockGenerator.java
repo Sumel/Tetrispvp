c0bf7cfce0181ccea5fca2545877ae7ef49c78cc
Kubster96 <jungiewiczjakub@gmail.com>
2018-01-18 15:27:23 +0100

    Block Generator changes

--- src/main/java/controller/BlockGenerator.java
+++ src/main/java/controller/BlockGenerator.java
@@ -3,29 +3,53 @@ package controller;
 import mocks.Block;
 import mocks.BlockManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BlockGenerator {
 
     private static BlockGenerator blockGenerator;
     private BlockManager blockManager;
 
-    public BlockGenerator() {
+    private List<Block> blockQueue;
+
+    public BlockGenerator(int queueSize) {
+
         this.blockManager = BlockManager.getBlockManager();
+        blockQueue = new ArrayList<>();
+
+        for(int i =0; i< queueSize; i++){
+            nextRandomBlock();
+        }
     }
 
-    public static BlockGenerator getBlockGenerator() {
+    public static BlockGenerator getBlockGenerator(int size) {
         if(blockGenerator == null) {
-            blockGenerator = new BlockGenerator();
+            blockGenerator = new BlockGenerator(size);
         }
         return blockGenerator;
-    }
-
-    public Block nextRandomBlock() {
-        return blockManager.randomBlock();
 
     }
 
-    public Block nextStraightLineBlock() {
-        return blockManager.straightBlock();
+    private void nextRandomBlock() {
+
+        blockQueue.add(blockManager.randomBlock());
+
+    }
+
+    public Block spawnBlock(){
+
+        Block block = blockQueue.remove(0);
+        nextRandomBlock();
+        return block;
+    }
+
+    public void nextStraightLineBlock(int numberOfLines) {
+
+        for(int i=0;i<numberOfLines; i++){
+            blockQueue.set(i, blockManager.straightBlock());
+
+        }
     }
 
 }
