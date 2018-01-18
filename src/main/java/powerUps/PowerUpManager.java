8b523ea4deaa8c74ec550328701cf05198aa7601
Anna Alberska <anna.alberska@wp.pl>
2018-01-18 15:32:09 +0100

    change controller

--- src/main/java/powerUps/PowerUpManager.java
+++ src/main/java/powerUps/PowerUpManager.java
@@ -13,7 +13,6 @@ public class PowerUpManager {
     private static PowerUpManager powerUpManager = null;
     private Random random = new Random();
     private MutableBoard board;
-    private GameController gameController;
     private MockNetwork mockNetwork;
     private LinesClearedListener linesClearedListener = new PowerUpLinesClearedListener();
 
@@ -66,14 +65,6 @@ public class PowerUpManager {
         	}
 
         }
-    }
-
-    public GameController getGameController() {
-        return gameController;
-    }
-
-    public void setGameController(GameController gameController) {
-        this.gameController = gameController;
     }
 
     public MutableBoard getBoard() {
